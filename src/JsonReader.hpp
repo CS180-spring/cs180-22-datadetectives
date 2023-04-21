33599bb006cf4c235cfef2782f53438be66bad75
Vinayak Gajjewar <vinayakgajjewar@gmail.com>
2023-04-21 11:26:41 -0700

    Modify class to return vector<json>

--- src/JsonReader.hpp
+++ src/JsonReader.hpp
@@ -1,6 +1,7 @@
 // JsonReader.cpp
 #include <iostream>
 #include <fstream>
+#include <vector>
 // https://github.com/nlohmann/json/blob/develop/single_include/nlohmann/json.hpp
 //#include <nlohmann/json.hpp>
 #include "../lib/json.hpp"
@@ -9,22 +10,21 @@ using json = nlohmann::json;
 
 class JsonReader {
  public:
-  std::string ReadFile(std::string file_path);
+  json ReadFile(std::string file_path);
 };
 
-std::string JsonReader::ReadFile(std::string file_path) {
+json JsonReader::ReadFile(std::string file_path) {
 
   // Open provided JSON file
   std::ifstream f(file_path);
   json data = json::parse(f);
 
-  // String to be returned
-  std::string return_val = "";
+  // Vector to be returned
+  std::vector<json> return_val;
 
   // Loop through values
   for (auto& element : data) {
-    return_val += element;
-    return_val += " ";
+    return_val.push_back(element);
   }
 
   return return_val;
--- test/JsonReaderTest.cpp
+++ test/JsonReaderTest.cpp
@@ -2,9 +2,13 @@
 
 #include "../src/JsonReader.hpp"
 #include <string>
+#include <vector>
+#include "../lib/json.hpp"
+
+using json = nlohmann::json;
 
 int main() {
     JsonReader my_json_reader = JsonReader();
-    std::string data = my_json_reader.ReadFile("/Users/vinayakgajjewar/Documents/Schoolwork/CS180/cs180-22-datadetectives/data/test.json");
-    std::cout << data << std::endl;
+    std::vector<json> data = my_json_reader.ReadFile("/Users/vinayakgajjewar/Documents/Schoolwork/CS180/cs180-22-datadetectives/data/test.json");
+    std::cout << data[0] << std::endl;
 }
