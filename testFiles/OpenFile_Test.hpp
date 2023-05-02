f3274942cb0e894204a6d7dcb21096f83ada27c0
Isiah Montalvo <SwiftZephyr17@gmail.com>
2023-05-02 04:14:42 -0700

    Ok this should be the one

--- testFiles/OpenFile_Test.hpp
+++ testFiles/OpenFile_Test.hpp
@@ -3,16 +3,20 @@
 
 #include "gtest/gtest.h"
 #include "../src/OpenFile.hpp"
+#include <string>
 
 TEST(OpenFile, RegularFile){
-    EXPECT_NO_THROW(openFile("../data/normal.csv"));
+    string csv_path = string(__FILE__).substr(0, string(__FILE__).find_last_of("/\\")) + "/../data/normal.csv";
+    EXPECT_NO_THROW(openFile(csv_path));
 }
 
 TEST(OpenFile, EmptyFile){
-    EXPECT_ANY_THROW(openFile("../data/blank.csv"));
+    string csv_path = string(__FILE__).substr(0, string(__FILE__).find_last_of("/\\")) + "/../data/blank.csv";
+    EXPECT_ANY_THROW(openFile(csv_path));
 }
 
 TEST(OpenFile, DoesNotExist){
-    EXPECT_ANY_THROW(openFile("../data/noFile.csv"));
+    string csv_path = string(__FILE__).substr(0, string(__FILE__).find_last_of("/\\")) + "/../data/noFile.csv";
+    EXPECT_ANY_THROW(openFile(csv_path));
 }
 #endif
