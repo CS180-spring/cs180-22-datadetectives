436a8a365fa14127fb47b74fbd48e4b110eeec4e
Isiah Montalvo <SwiftZephyr17@gmail.com>
2023-05-02 02:09:55 -0700

    Another test maybe don't merge idk if it breaks anything yet.

---
+++ x64-Debug/googletest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Swift/source/repos/cs180-22-datadetectives/googletest/googlemock
+# Build directory: C:/Users/Swift/source/repos/cs180-22-datadetectives/x64-Debug/googletest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
---
+++ x64-Debug/googletest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Swift/source/repos/cs180-22-datadetectives/googletest/googletest
+# Build directory: C:/Users/Swift/source/repos/cs180-22-datadetectives/x64-Debug/googletest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
