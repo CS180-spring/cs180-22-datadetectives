0ec8cc29b0f68814f8bc9e9514167ebe358c277e
AdamCarp <adamcarp24@gmail.com>
2023-05-04 16:11:27 -0700

    Combines Mappers/Fix function names

--- src/Concurrency.hpp
+++ src/Concurrency.hpp
@@ -10,9 +10,12 @@
 #include <map>
 #include <iostream>
 
-struct outputs : std::vector<std::vector<std::pair<std::string, int>>> {
-    outputs() : std::vector<std::vector<std::pair<std::string, int>>>() {}
-    void new_member (std::vector<std::pair<std::string, int>> n) { push_back(n); }
+struct outputs : std::vector<std::pair<std::string, int>> {
+    outputs() : std::vector<std::pair<std::string, int>>() {}
+    //combines map returns
+    void new_member (std::vector<std::pair<std::string, int>> n) {
+        insert(end(), std::make_move_iterator(n.begin()), std::make_move_iterator(n.end()));
+    }
 };
 
 struct reducerOutputs : std::map<std::string, int> {
@@ -49,7 +52,7 @@ std::map<std::string, int> Concurrency::runMapReduce(IMapReduce userMapReduce, s
     //run mappers concurrently
     std::vector<std::thread> mappers;
     for(int i = 0; i < mapThreadCount; ++i){
-        mappers.push_back(std::thread(&outputs::new_member, std::ref(mapper_outputs), map_reducer_engine.Run(splitData[i])));
+        mappers.push_back(std::thread(&outputs::new_member, std::ref(mapper_outputs), map_reducer_engine.Map(splitData[i])));
     }
 
     //close threads
@@ -57,13 +60,13 @@ std::map<std::string, int> Concurrency::runMapReduce(IMapReduce userMapReduce, s
 
     //shuffle completed maps
     //and split up for reducers
-    std::vector<std::map<std::string, std::vector<int>>> splitMaps = splitter.splitMap(map_reducer_engine.shuffle(mapper_outputs));
+    std::vector<std::map<std::string, std::vector<int>>> splitMaps = splitter.splitMap(map_reducer_engine.Shuffle(mapper_outputs));
 
     //create reducer threads by loop
     //run reducers concurrently
     std::vector<std::thread> reducers;
     for(int i = 0; i < redThreadCount; ++i){
-        reducers.push_back(std::thread(&reducerOutputs::new_member, std::ref(reducer_outputs), map_reducer_engine.reduce(splitMaps[i])));
+        reducers.push_back(std::thread(&reducerOutputs::new_member, std::ref(reducer_outputs), map_reducer_engine.Reduce(splitMaps[i])));
     }
 
     //close threads
