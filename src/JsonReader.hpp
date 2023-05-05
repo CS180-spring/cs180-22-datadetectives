9683651bd7b07e92a03ae99e3d76d2d8b7ef909f
Diep, Jason <jdiep16@go.pasadena.edu>
2023-05-05 01:26:40 -0700

    changed both readers to take in multiple files at one time

--- src/JsonReader.hpp
+++ src/JsonReader.hpp
@@ -1,12 +1,7 @@
-// JsonReader.hpp
-
-#ifndef MAPREDUCE_SRC_JSONREADER_H_
-#define MAPREDUCE_SRC_JSONREADER_H_
-
+// JsonReader.cpp
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include <sstream>
 // https://github.com/nlohmann/json/blob/develop/single_include/nlohmann/json.hpp
 //#include <nlohmann/json.hpp>
 #include "../lib/json.hpp"
@@ -15,33 +10,74 @@ using json = nlohmann::json;
 
 class JsonReader {
  public:
-  std::vector<std::string> ReadFile(std::string file_path);
+  std::vector<std::string> ReadFile(std::vector<std::string> v);
+  void TraverseJson(json data, std::vector<std::string>& temp);
 };
 
-std::vector<std::string> JsonReader::ReadFile(std::string file_path) {
+std::vector<std::string> JsonReader::ReadFile(std::vector<std::string> v) {
 
-  // Open provided JSON file
-  std::ifstream f(file_path);
-  json data = json::parse(f);
-
-  // Vector to be returned
   std::vector<std::string> return_val;
 
-  // Loop through values
-  for (auto& element : data) {
+  for(int i = 0; i < v.size(); i++){
+    //read through the vector for filenames
+    std::ifstream f(v.at(i));
+    json data = json::parse(f);
 
-    std::stringstream ss;
-    ss.str(element);
-    std::string word;
+    //traverse through one json file and hold values
+    // from the file and append to total vector string
+    std::vector<std::string> temp;
+    TraverseJson(data, temp);
+    return_val.insert(return_val.end(), temp.begin(), temp.end());
 
-    while (ss >> word) {
-      word.erase(std::remove_if(word.begin(), word.end(), [](char c) { return !std::isalpha(c); }), word.end());
-      std::transform(word.begin(), word.end(), word.begin(), [](char c) { return std::tolower(c); });
-      return_val.push_back(word);
-    }
+    f.close();
   }
-
   return return_val;
+
+  // single file 
+
+  // // Open provided JSON file
+  // std::ifstream f(file_path);
+  // json data = json::parse(f);
+
+  // // Vector to be returned
+  // std::vector<std::string> return_val;
+
+  // // Loop through values
+  // for (auto& element : data) {
+  //   return_val.push_back(element);
+  // }
+
+  // return return_val;
 }
 
-#endif
+void JsonReader::TraverseJson(json data, std::vector<std::string>& temp){
+  for(auto& element : data.items()){
+    if(element.value().is_object()){
+      TraverseJson(element.value(), temp);
+    }
+    else if(element.value().is_array()){
+      for(auto& item : element.value()){
+        if(item.is_object()){
+          TraverseJson(item, temp);
+        }
+        else if(item.is_array()){
+          TraverseJson(item, temp);
+        }
+        else if(item.is_number()){
+          temp.push_back(std::to_string(static_cast<int>(item)));
+        }
+        else if(item.is_string()){
+          temp.push_back(item);
+        }
+      }
+    }
+    else{
+      if(element.value().is_number()){
+        temp.push_back(std::to_string(static_cast<int>(element.value())));
+      }
+      else if(element.value().is_string()){
+        temp.push_back(element.value());
+      }
+    }
+  }
+}
