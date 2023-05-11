b5910f953006f2b335f56f129906d22b2147a057
Vinayak Gajjewar <vinayakgajjewar@gmail.com>
2023-05-11 10:51:25 -0700

    Fix some stuff in the MapReduceEngine.

--- src/MapReduceEngine.hpp
+++ src/MapReduceEngine.hpp
@@ -18,32 +18,29 @@ class MapReduceEngine {
   // Run function
   std::vector<std::pair<std::string, int>> Run(const std::vector<std::string>& input_data) {
 
-    // Map phase
-    std::vector<std::pair<std::string, int>> map_outputs;
-    for (const auto& record : input_data) {
-      map_outputs.push_back(map_reduce_.Map(record));
-    }
+    // Run the map stage.
+    std::vector<std::pair<std::string, int>> map_outputs = this->Map(input_data);
 
     // Print map results
     for (const auto& record : map_outputs) {
       std::cout << record.first << ": " << record.second << std::endl;
     }
 
-    return map_outputs;
-  }
+    // Execute the shuffle stage.
+    std::map<std::string, std::vector<int>> shuffle_outputs = this->Shuffle(map_outputs);
 
-  std::map<std::string, int> Reduce(const std::map<std::string, std::vector<int>> &pairedValues){
-    std::string key = pairedValues.begin()->first;
-    std::vector<int> values = pairedValues.at(key);
+    // Execute the reduce stage.
+    std::map<std::string, int> reduce_outputs = this->Reduce(shuffle_outputs);
 
-    int total = 0;
-    for(int i = 0; i < values.size(); i++){
-        total += values[i];
+    // Print reduce outputs.
+    std::cout << "------------------" << std::endl;
+    std::cout << "| REDUCE OUTPUTS |" << std::endl;
+    std::cout << "------------------" << std::endl;
+    for (const auto& [key, value] : reduce_outputs) {
+      std::cout << key << std::endl;
+      std::cout << "\t" << value << std::endl;
+      std::cout << std::endl;
     }
-
-    std::map<std::string, int> reducedKeyValue;
-    reducedKeyValue[key] = total;
-    return reducedKeyValue; 
   }
 
   // Map function.
@@ -57,9 +54,9 @@ class MapReduceEngine {
     }
 
     // Print map results.
-    std::cout << "--------" << std::endl;
-    std::cout << "MAP OUTPUTS" << std::endl;
-    std::cout << "--------" << std::endl;
+    std::cout << "---------------" << std::endl;
+    std::cout << "| MAP OUTPUTS |" << std::endl;
+    std::cout << "---------------" << std::endl;
     for (const auto& record : map_outputs) {
       std::cout << record.first << ": " << record.second << std::endl;
     }
@@ -79,9 +76,9 @@ class MapReduceEngine {
     }
 
     // Print shuffle phase outputs.
-    std::cout << "--------" << std::endl;
-    std::cout << "SHUFFLE OUTPUTS" << std::endl;
-    std::cout << "--------" << std::endl;
+    std::cout << "-------------------" << std::endl;
+    std::cout << "| SHUFFLE OUTPUTS |" << std::endl;
+    std::cout << "-------------------" << std::endl;
     for (const auto& [key, value] : shuffled_outputs) {
       std::cout << key << std::endl;
       std::cout << "\t";
@@ -92,6 +89,23 @@ class MapReduceEngine {
     }
 
     return shuffled_outputs;
+  }
+
+  // Reduce function.
+  // Aggregate the values for each key using a user-define reduce function.
+  // TODO we need to let the user specify this!
+  std::map<std::string, int> Reduce(const std::map<std::string, std::vector<int>> &pairedValues){
+    std::string key = pairedValues.begin()->first;
+    std::vector<int> values = pairedValues.at(key);
+
+    int total = 0;
+    for(int i = 0; i < values.size(); i++){
+        total += values[i];
+    }
+
+    std::map<std::string, int> reducedKeyValue;
+    reducedKeyValue[key] = total;
+    return reducedKeyValue; 
   }
  
  private:
