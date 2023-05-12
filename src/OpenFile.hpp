5f12ef2b59c6d828d2c8d929245fe998afd72d4f
jlgardea <103074913+jlgardea@users.noreply.github.com>
2023-05-12 11:24:29 -0700

    Open multiple Files

--- src/OpenFile.hpp
+++ src/OpenFile.hpp
@@ -6,11 +6,11 @@ using namespace std;
 class OpenFile{
     public:
     ifstream openFile(string inputFile);
+    void openFile(vector<string> inputFiles);  
 };
 
 ifstream OpenFile::openFile(std::string inputFile){
     ifstream iFile(inputFile);
-    string s;
 
     if(!iFile){
         throw runtime_error("Error: Unable to open file " +  inputFile);
@@ -21,5 +21,21 @@ ifstream OpenFile::openFile(std::string inputFile){
     }
     else{
         return iFile;
+    }
+}
+
+void openFile(vector<string> inputFiles){
+
+    for (int i = 0; i < inputFiles.size(); i++){
+        ifstream iFile(inputFiles[i]);
+
+        if(!iFile){
+            throw runtime_error("Error: Unable to open file " +  inputFiles[i]);
+        }
+
+        if(iFile.peek() == ifstream::traits_type::eof()){
+            throw runtime_error("Error: " +  inputFiles[i] + " is empty.");
+        }
+
     }
 }
