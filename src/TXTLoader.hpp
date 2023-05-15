34b0327712d625b8769b2d1035e1013a6893b852
Diep, Jason <jdiep16@go.pasadena.edu>
2023-05-15 13:20:12 -0700

    quick txtloader

--- src/TXTLoader.hpp
+++ src/TXTLoader.hpp
@@ -1,9 +1,24 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <vector>
 
 using namespace std;
 
 class TXTloader{
 public:
+    vector<string> loadTXT(vector<string> v){
+        string s;
+        vector<string> lines; 
+        for(int i = 0; i < v.size(); i++){
+            ifstream f(v.at(i));
+            while(getline(f, s)){
+                if(s.length() != 0){
+                    lines.push_back(s);
+                }
+            }
+            f.close();
+        }
+        return lines;
+    }
 };
