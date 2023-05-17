f09f16a06c609bdc7880a0cc97a5b378b6e0fc19
AdamCarp <adamcarp24@gmail.com>
2023-05-16 18:16:55 -0700

    splitMap test cases

--- testFiles/csvSplit_Test.hpp
+++ testFiles/csvSplit_Test.hpp
@@ -55,4 +55,60 @@ TEST(SplitTest, IndivisiblySized){
     EXPECT_EQ(testVec.size(), sum);
 }
 
+TEST(splitMap, Divisible){
+    map<string, vector<int> > mymap;
+
+    vector<int> vect;
+    vect.push_back(1);
+    vect.push_back(1);
+
+    mymap["a"] = vect;
+    mymap["b"] = vect;
+    mymap["c"] = vect;
+    mymap["d"] = vect;
+    mymap["e"] = vect;
+    mymap["f"] = vect;
+    mymap["g"] = vect;
+    mymap["h"] = vect;
+    csvSplitter splitter(4,4);
+    vector<map<string, vector<int>>> result = splitter.splitMap(mymap);
+
+    EXPECT_EQ(result[0].size(), result[1].size());
+    EXPECT_EQ(result[2].size(), result[3].size());
+}
+
+TEST(splitMap, Indivisible){
+    map<string, vector<int> > mymap;
+
+    vector<int> vect;
+    vect.push_back(1);
+    vect.push_back(1);
+
+    mymap["a"] = vect;
+    mymap["b"] = vect;
+    mymap["c"] = vect;
+    mymap["d"] = vect;
+    mymap["e"] = vect;
+    mymap["f"] = vect;
+    mymap["g"] = vect;
+    mymap["h"] = vect;
+    mymap["i"] = vect;
+    csvSplitter splitter(4,4);
+    vector<map<string, vector<int>>> result = splitter.splitMap(mymap);
+
+    EXPECT_EQ(result[0].size(), result[1].size());
+    EXPECT_EQ(result[3].size(), 3);
+}
+
+TEST(splitMap, EmptyInput){
+    map<string, vector<int> > mymap;
+    csvSplitter splitter(4,4);
+    vector<map<string, vector<int>>> result = splitter.splitMap(mymap);
+
+    //creates sub-maps
+    EXPECT_EQ(result.size(), 4);
+    //that are empty
+    EXPECT_EQ(result[0].size(), 0);
+}
+
 #endif //CSVSPLIT_TEST_HPP
