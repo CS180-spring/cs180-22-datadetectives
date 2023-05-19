7c9a3b0616a0b9762f9176800a25e963c82e17f9
Vinayak Gajjewar <vinayakgajjewar@gmail.com>
2023-05-18 21:52:14 -0700

    Fix bug.

--- src/OutputSorter.hpp
+++ src/OutputSorter.hpp
@@ -15,25 +15,26 @@ class OutputSorter {
 
   /*
    * Utility method to sort a map by key.
+   * This function returns a vector of pairs with each element of the vector in the specified sort order.
    */
-  std::vector<std::pair<int, std::string>> sortByKey(const std::map<std::string, int>&);
+  std::vector<std::pair<std::string, int>> sortByKey(const std::map<std::string, int>&);
 };
 
 /*
  * Implementation of sortByKey().
  */
-std::vector<std::pair<int, std::string>> OutputSorter::sortByKey(const std::map<std::string, int>& my_map) {
+std::vector<std::pair<std::string, int>> OutputSorter::sortByKey(const std::map<std::string, int>& my_map) {
 
   /*
    * Convert the map to a vector of pairs.
    */
-  std::vector<std::pair<int, std::string>> pair_vec;
+  std::vector<std::pair<std::string, int>> pair_vec;
   std::copy(my_map.begin(), my_map.end(), std::back_inserter(pair_vec));
 
   /*
    * Sort the vector of pairs based on the first element of each pair.
    */
-  std::sort(pair_vec.begin(), pair_vec.end(), [](const std::pair<int, std::string>& a, const std::pair<int, std::string>& b) {return a.first > b.first;});
+  std::sort(pair_vec.begin(), pair_vec.end(), [](const std::pair<std::string, int>& a, const std::pair<std::string, int>& b) {return a.first.compare(b.first) < 0;});
 
   return pair_vec;
 }
