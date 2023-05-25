ba7d8165530074a167ad94875af4291854924e5d
Vinayak Gajjewar <vinayakgajjewar@gmail.com>
2023-05-24 17:08:47 -0700

    Implement FilterValuesOutsideRange.

--- src/Filter.hpp
+++ src/Filter.hpp
@@ -23,7 +23,8 @@ class Filter {
   void FilterValuesGreaterThan(std::map<std::string, int>&, int);
 
   /*
-   * Filter out all records that aren't within a given range.
+   * Filter out all records that aren't within a given range. The first int
+   * argument is the lower bound and the second int argument is the upper bound.
    */
   void FilterValuesOutsideRange(std::map<std::string, int>&, int, int);
 
@@ -67,6 +68,25 @@ void Filter::FilterValuesGreaterThan(
    */
   for (auto my_iter = my_map.begin(); my_iter != my_map.end();) {
     if (my_iter->second > upper_bound) {
+      my_iter = my_map.erase(my_iter);
+    } else {
+      my_iter++;
+    }
+  }
+}
+
+void Filter::FilterValuesOutsideRange(
+  std::map<std::string, int>& my_map,
+  int lower_bound,
+  int upper_bound
+) {
+
+  /*
+   * Iterate through the map and erase any records that don't satisfy the
+   * condition.
+   */
+  for (auto my_iter = my_map.begin(); my_iter != my_map.end();) {
+    if (my_iter->second < lower_bound || my_iter->second > upper_bound) {
       my_iter = my_map.erase(my_iter);
     } else {
       my_iter++;
