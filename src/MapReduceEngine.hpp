84f11d1b917b0c79d62300904bb653efea525929
Vinayak Gajjewar <vinayakgajjewar@gmail.com>
2023-05-30 12:10:26 -0700

    Move the run function to its own file.

--- src/MapReduceEngine.hpp
+++ src/MapReduceEngine.hpp
@@ -20,53 +20,27 @@ class MapReduceEngine {
   MapReduceEngine(IMapReduce& map_reduce) : map_reduce_(map_reduce) {}
 
   // Run function
-  std::map<std::string, int> Run(const std::vector<std::string>& input_data) {
+  std::map<std::string, int> Run(const std::vector<std::string>& input_data);
 
-    // Run the map stage.
-    std::vector<std::pair<std::string, int>> map_outputs = this->Map(input_data);
-
-    // Execute the shuffle stage.
-    std::map<std::string, std::vector<int>> shuffle_outputs = this->Shuffle(map_outputs);
-
-    // Execute the reduce stage.
-    std::map<std::string, int> reduce_outputs = this->Reduce(shuffle_outputs);
+  /*
+   * Map function definition. All we do here is just run the user-defined map
+   * function on each input record and emit the result.
+   */
+  std::vector<std::pair<std::string, int>> Map(
+    const std::vector<std::string>& input_data
+  ) {
 
     /*
-     * Return the output of the reduce operation.
+     * Run the user-defined Map operation on each input record.
      */
-    return reduce_outputs;
-  }
-
-  // Map function.
-  // TODO: write a good description.
-  std::vector<std::pair<std::string, int>> Map(const std::vector<std::string>& input_data) {
-
-    auto start_time = std::chrono::high_resolution_clock::now();
-
-    // Map phase
     std::vector<std::pair<std::string, int>> map_outputs;
     for (const auto& record : input_data) {
       map_outputs.push_back(map_reduce_.Map(record));
     }
 
-    // Print map results.
     /*
-    std::cout << "---------------" << std::endl;
-    std::cout << "| MAP OUTPUTS |" << std::endl;
-    std::cout << "---------------" << std::endl;
-    for (const auto& record : map_outputs) {
-      std::cout << record.first << ": " << record.second << std::endl;
-    }
-    */
-/*
-    // This line waits for 1 second.
-    // For testing purposes only.
-    std::this_thread::sleep_until(std::chrono::system_clock::now() + std::chrono::nanoseconds(1405619385));
-
-    auto end_time = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
-    std::cout << "Map stage duration: " << duration.count() << " ms" << std::endl;
-*/
+     * Return the output of the Map operation.
+     */
     return map_outputs;
   }
 
@@ -87,30 +61,6 @@ class MapReduceEngine {
       shuffled_outputs[pair.first].push_back(pair.second);
     }
 
-    // Print shuffle phase outputs.
-    /*
-    std::cout << "-------------------" << std::endl;
-    std::cout << "| SHUFFLE OUTPUTS |" << std::endl;
-    std::cout << "-------------------" << std::endl;
-    for (const auto& [key, value] : shuffled_outputs) {
-      std::cout << key << std::endl;
-      std::cout << "\t";
-      for (const auto& elem : value) {
-        std::cout << elem << " ";
-      }
-      std::cout << std::endl;
-    }
-    */
-
-/*
-    // This line waits for 1 second.
-    // For testing purposes only.
-    std::this_thread::sleep_until(std::chrono::system_clock::now() + std::chrono::nanoseconds(9481958320));
-
-    auto end_time = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
-    std::cout << "Shuffle stage duration: " << duration.count() << " ms" << std::endl;
-*/
     return shuffled_outputs;
   }
 
@@ -125,26 +75,6 @@ class MapReduceEngine {
       reduce_outputs.emplace(pair.first, map_reduce_.Reduce(pair.first, pair.second));
     }
 
-/*
-    // Print reduce outputs.
-    std::cout << "------------------" << std::endl;
-    std::cout << "| REDUCE OUTPUTS |" << std::endl;
-    std::cout << "------------------" << std::endl;
-    for (const auto& [key, value] : reduce_outputs) {
-      std::cout << key << std::endl;
-      std::cout << "\t" << value << std::endl;
-      std::cout << std::endl;
-    }
-*/
-/*
-    // This line waits for 1 second.
-    // For testing purposes only.
-    std::this_thread::sleep_until(std::chrono::system_clock::now() + std::chrono::nanoseconds(3912093476));
-
-    auto end_time = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
-    std::cout << "Reduce stage duration: " << duration.count() << " ms" << std::endl;
-*/
     return reduce_outputs;
   }
  
